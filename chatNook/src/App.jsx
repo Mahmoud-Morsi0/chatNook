22e6929e468f67a480f3b63c772e149cbc17d821
Morsi <136155900+Mahmoud-Morsi0@users.noreply.github.com>
2024-03-14 14:38:53 +0200

    chatHeader, ChatMessage and ChatFooter

--- chatNook/src/App.jsx
+++ chatNook/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-
 import './App.css'
-
+import Message from './components/chatMessage';
+import ChatHeader from './components/chatHeader';
+import ChatFooter from './components/chatFooter';
 function App() {
 
   return (
     
-   <div>
-     <h1 className="text-3xl font-bold underline">
-      Hello at our ChatNook
-    </h1>
+   <div className='bg-white h-screen w-3/4 ms-auto'>
+    <ChatHeader/>
+    <Message/>
+    <Message/>
+    <ChatFooter/>
    </div>
   )
 }

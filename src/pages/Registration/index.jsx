08b6c970b79cbcc978a921b6ef79a2240d856dcd
nour fawzy <nourfawzy292001@gmail.com>
2024-03-18 08:30:55 +0200

    modify in signup page and make page responsive

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -1,152 +1,170 @@
-import { userValidationSchema } from "../../schema/shemaValidation";
 import SignWithGmailBtn from "../../components/SigninWithGoogle";
 import SectionBreak from "../../components/SectionBreak";
 import { Link } from "react-router-dom";
+import React from "react";
 import { useFormik } from "formik";
-import { registration } from "../../api/auth";
-
+// import { registration } from "../../api/auth";
+import { userValidationSchema } from "../../schema/shemaValidation";
+import axios from "axios";
 
 const Registration = () => {
-  let loaging=true
-  const onSubmit = async (values,actions) => {
-    loaging=true
-    // await new Promise((resolve) => setTimeout(resolve, 1000));
-    console.log(values);
-    const repsonse = await registration(values);
-    
-    console.log(response);
-    actions.resetForm();
-    loaging=false
+  // let loaging = true;
+  // const onSubmit = async (values, actions) => {
+  //   // loaging = true;
+  //   console.log(values);
+  // const repsonse = await registration(values);
+  //   console.log(repsonse);
+  // actions.resetForm();
+  // loaging = false;
+  // // const sendDataToApi = async (values) => {
+  // //   try {
+  // //     const response = await axios.post(
+  // //       "https://note-sigma-black.vercel.app/api/v1/users/signUp",
+  // //       values,{
+  //       // headers: {
+  //       //   'Content-Type': 'application/json' // Specify the content type as JSON
+  //       // }
+  //     }
+  //     );
+  //     console.log(response.data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
-  };
-
-  const {
-    values,
-    handleChange,
-    handleBlur,
-    handleSubmit,
-    errors,
-    touched,
-    isSubmitting,
-  } = useFormik({
+  const formik = useFormik({
     initialValues: {
-      fullName: "",
+      name: "",
       email: "",
       password: "",
     },
-    // validationSchema: userValidationSchema,
-    onSubmit: (values) => {
-      onSubmit(values);
-    },
+    validationSchema: userValidationSchema,
+    onSubmit: sendDataToApi,
   });
+
   return (
-    <section className="w-full h-full p-12">
-      <div className="flex flex-row w-5/6 m-auto">
-        <div className="left-sec pt-20 w-1/2">
-          <img
-            className="w-4/6 m-auto"
-            src="login img/Speech bubbles-amico 1.png"
-            alt=""
-          />
-        </div>
-        <div className="right-sec w-1/2">
-          <div className="header-text text-cyan-700 font-bold text-3xl m-auto text-center mb-5 ">
-            Join With Our Community
+    <>
+      <section className="py-14">
+        <div className="xl:grid grid-cols-2 gap-4 lg:grid grid-cols-1">
+          <div className="left-sec flex  justify-center ">
+            <img
+              className="w-4/6"
+              src="login img/Speech bubbles-amico 1.png"
+              alt="signup"
+            />
           </div>
-          <p className="sub-heading text-gray-400 text-sm text-center block mb-7 font-medium">
-            Help you to contact everyone with just easy way
-          </p>
-          <SignWithGmailBtn />
-          <SectionBreak />
-          <div className="form-sec ps-28 w-11/2 mb-10 ">
-            <form onSubmit={handleSubmit}>
-              <div>
-                <label
-                  htmlFor="name"
-                  className="block text-sm ml-3 font-medium leading-6 text-gray-900"
-                >
-                  Full Name
-                </label>
-                <input
-                  type="text"
-                  name="fullName"
-                  id="name"
-                  value={values.fullName}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={`block w-5/6 h-10 rounded-md ml-3 mt-2 mb-2 py-1 pl-4 pr-20 text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-200 placeholder:text-gray-400 sm:text-sm sm:leading-6 bg-gray-100 ${
-                    errors.fullName && touched.fullName ? "outline-red-500" : ""
-                  }`}
-                />
-                {errors.fullName && touched.fullName && (
-                  <p className="ms-5 text-red-600">{errors.fullName}</p>
-                )}
-
-                <label
-                  htmlFor="email"
-                  className="block text-sm ml-3 font-medium leading-6 text-gray-900"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  value={values.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={`block w-5/6 h-10 rounded-md ml-3 mt-2 mb-2 py-1 pl-4 pr-20 text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-200 placeholder:text-gray-400 sm:text-sm sm:leading-6 bg-gray-100 ${
-                    errors.email && touched.email ? "outline-red-500" : ""
-                  }`}
-                />
-                {errors.email && touched.email && (
-                  <p className="ms-5 text-red-600">{errors.email}</p>
-                )}
-
-                <label
-                  htmlFor="password"
-                  className="block text-sm ml-3 font-medium leading-6 text-gray-900"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  value={values.password}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={`block w-5/6 h-10 rounded-md ml-3 mt-2 mb-2 py-1 pl-4 pr-20 text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-200 placeholder:text-gray-400 sm:text-sm sm:leading-6 bg-gray-100 ${
-                    errors.password && touched.password ? "outline-red-500" : ""
-                  }`}
-                />
-                {errors.password && touched.password && (
-                  <p className="ms-5 text-red-600">{errors.password}</p>
-                )}
-              </div>
-              <button
-                type="submit"
-                disabled={isSubmitting}
-                className={`text-white bg-cyan-800 border-2 border-cyan-800 border-solid rounded-md w-64 h-12 font-medium hover:bg-white hover:text-cyan-800 mx-4`}
-              >
-                Sign up
-              </button>
-            </form>
-          </div>
-          <div className="m-auto w-72 text-center">
-            <p className="text-base text-gray-700 mt-2">
-              Already a member?
-              <Link
-                className=" cursor-pointer font-bold text-[#1e7887]"
-                to="/login"
-              >
-                LOG IN
-              </Link>{" "}
+          <div className="right-sec">
+            <div className="header-text text-cyan-700 font-bold text-3xl m-auto text-center mb-5 ">
+              Join With Our Community
+            </div>
+            <p className="sub-heading text-gray-400 text-sm text-center block mb-7 font-medium">
+              Help you to contact everyone with just easy way
             </p>
+            <SignWithGmailBtn />
+            <SectionBreak />
+            <div className="form-sec mb-10 flex flex-col items-center">
+              <form onSubmit={formik.handleSubmit} className="w-full max-w-md">
+                <div className=" w-full flex flex-col justify-center mb-6">
+                  <label
+                    htmlFor="name"
+                    className="block text-sm ml-3 font-medium leading-6 text-gray-900"
+                  >
+                    {" "}
+                    name
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="fullName"
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    className="input w-11/12 text-gray-900 focus:outline-none grow focus:ring-2 focus:ring-gray-200 placeholder:text-gray-400 sm:text-sm sm:leading-6 bg-gray-100 focus:bg-gray-10"
+                  />
+                  {formik.errors.name && formik.touched.name ? (
+                    <div className=" w-11/12 text-red-600">
+                      {formik.errors.name}
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <div className=" w-full flex flex-col justify-center mb-6">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm ml-3 font-medium leading-6 text-gray-900"
+                  >
+                    {" "}
+                    Email
+                  </label>
+                  <input
+                    type="text"
+                    name="email"
+                    id="name"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    className="input w-11/12 text-gray-900 focus:outline-none grow focus:ring-2 focus:ring-gray-200 placeholder:text-gray-400 sm:text-sm sm:leading-6 bg-gray-100 focus:bg-gray-100"
+                  />
+                  {formik.errors.email && formik.touched.email ? (
+                    <div className="w-11/12 text-red-600">
+                      {formik.errors.email}
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <div className=" w-full flex flex-col justify-center mb-6">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm ml-3 font-medium leading-6 text-gray-900"
+                  >
+                    {" "}
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    className="input w-11/12 text-gray-900 focus:outline-none grow focus:ring-2 focus:ring-gray-200 placeholder:text-gray-400 sm:text-sm sm:leading-6 bg-gray-100 focus:bg-gray-100"
+                  />
+                  {formik.errors.password && formik.touched.password ? (
+                    <div className=" w-11/12 text-red-600">
+                      {formik.errors.password}
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <div className="w-full flex flex-col justify-center">
+                  <button
+                    type="submit"
+                    disabled={!(formik.isValid && formik.dirty)}
+                    className="text-white bg-cyan-800 border-2 m-auto border-cyan-800 border-solid rounded-md btn w-2/3 font-medium hover:bg-white hover:text-cyan-800 "
+                  >
+                    Sign up
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div className="m-auto w-72 text-center">
+              <p className="text-base text-gray-700 mt-2">
+                Already a member?
+                <Link
+                  className=" cursor-pointer font-bold text-[#1e7887]"
+                  to="/login"
+                >
+                  LOG IN
+                </Link>{" "}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
--- src/schema/shemaValidation.js
+++ src/schema/shemaValidation.js
@@ -1,21 +1,23 @@
-import * as yup from 'yup'
+import * as yup from "yup";
 export const userValidationSchema = yup.object().shape({
-    fullName: yup.string()
-        .required("Full name is required")
-        .min(3, "Full name should be at least 3 characters long")
-        .max(25, "Full name should not exceed 25 characters"),
+  name: yup
+    .string()
+    .required("Full name is required")
+    .min(3, "Full name should be at least 3 characters long")
+    .max(25, "Full name should not exceed 25 characters"),
 
-    email: yup.string()
-        .email("Invalid email format")
-        .required("Email is required"),
+  email: yup
+    .string()
+    .email("Invalid email format")
+    .required("Email is required"),
 
-    password: yup.string()
-        .required("Password is required")
-        .min(8, "Password should be at least 8 characters long")
-        .matches(
-            /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!#%?&])[A-Za-z\d@$!#%?&]{8,}$/,
-            "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
-        ),
-    profilePic: yup.string()
-        .default('')
+  password: yup
+    .string()
+    .required("Password is required")
+    .min(8, "Password should be at least 8 characters long")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+    ),
+  profilePic: yup.string().default(""),
 });

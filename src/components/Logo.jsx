d5fc184213b42055313ba48bbb6fde81a7005e75
nour fawzy <nourfawzy292001@gmail.com>
2024-03-18 09:11:41 +0200

    modify in navbar and try to make responsive

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import myImage from "../assets/Logo.png";
 export default function Logo() {
   return (
     <>
-      <img src={myImage} alt="" width="100px" className="mt-4 ms-6" />
+      <img src={myImage} alt="" width="100px" className="mt-4 " />
     </>
   );
 }
